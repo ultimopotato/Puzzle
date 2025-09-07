34f671facbf77a0f3e4e8e7b2934cadef124b0b0
DESKTOP-UG9P139\mainpc <ultimopotatosaga@gmail.com>
2025-09-08 08:39:03 +0900

    1.카드돌리는거 까지 시현

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Card : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private SpriteRenderer CardRenderer;
+
+    [SerializeField]
+    private Sprite animalSprite;
+
+
+    [SerializeField]
+    private Sprite backSprite;
+
+    private bool isFlipped= false;
+    private bool isFlipping=false;
+
     void Start()
     {
         
@@ -14,8 +27,47 @@ public class Card : MonoBehaviour
         
     }
 
+    public void FlipCard()
+    {
+
+        isFlipping = true;
+
+        Vector3 originalScale = transform.localScale;
+        Vector3 targetScale = new Vector3(0f, originalScale.y, originalScale.z);
+
+        transform.DOScale(targetScale, 0.2f).OnComplete(() =>
+        {
+            isFlipped = !isFlipped;
+
+            if (isFlipped)
+            {
+                CardRenderer.sprite = animalSprite;
+            }
+            else
+            {
+                CardRenderer.sprite = backSprite;
+            }
+
+            transform.DOScale(originalScale, 0.2f).OnComplete(() => 
+            {
+                isFlipping = false;
+            }
+            );
+
+        }
+        );
+
+        
+
+
+        
+    }
+
     void OnMouseDown()
     {
-        Debug.Log("mouse down");    
+        if(!isFlipping)  //if(isFlipping==false){}랑 같은말
+        {  
+            FlipCard();   
+        }
     }
 }
